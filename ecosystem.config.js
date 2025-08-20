35551c154119dd9bfffa9a0731812736a5779c25
Olwethu Ngalo <gabavane626@gmail.com>
2025-08-19 17:19:03 -0700

    Initial static site commit

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps : [{
+     name   : "MyWebsiteOdz",
+    script : "C:\\Users\\pc\\AppData\\Roaming\\npm\\http-server.cmd",
+    args   : "",
+    cwd    : "C:\\Users\\pc\\Documents\\my-nextjs-app\\website"
+  }]
+};
